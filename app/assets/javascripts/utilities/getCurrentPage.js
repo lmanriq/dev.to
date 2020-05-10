493e2e8977df25897c7e2657a558a00770f1b21a
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-10 13:27:50 -0600

    Add annotation to show current page data label

--- app/assets/javascripts/utilities/getCurrentPage.js
+++ app/assets/javascripts/utilities/getCurrentPage.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// When we are on the dashboards page, that page has this attribute: data-current-page="dashboards-show"
+
 function getCurrentPage(classString) {
   return (
     document.querySelectorAll("[data-current-page='" + classString + "']")
