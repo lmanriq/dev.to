9983136b163a5be2a350435132237723307c6860
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-10 13:46:04 -0600

    Add annotations for the pro analytics section that can be modified to show our stats

--- app/javascript/analytics/dashboard.js
+++ app/javascript/analytics/dashboard.js
@@ -1,19 +1,22 @@
 import { callHistoricalAPI, callReferrersAPI } from './client';
 
 function resetActive(activeButton) {
+  // Selects all of the time range buttons and removes the selected class
   const buttons = document.getElementsByClassName('timerange-button');
   for (let i = 0; i < buttons.length; i += 1) {
     const button = buttons[i];
     button.classList.remove('selected');
   }
-
+  // Adds the selected class to the button being clicked on 
   activeButton.classList.add('selected');
 }
 
+// Returns the sum of whatever datapoint is being passed in 
 function sumAnalytics(data, key) {
   return Object.entries(data).reduce((sum, day) => sum + day[1][key].total, 0);
 }
 
+// Displays a header and stat 
 function cardHTML(stat, header) {
   return `
     <h4>${header}</h4>
@@ -22,22 +25,27 @@ function cardHTML(stat, header) {
 }
 
 function writeCards(data, timeRangeLabel) {
+  // Finds the sum analystics for page_views, reactions, comments, and follows
   const readers = sumAnalytics(data, 'page_views');
   const reactions = sumAnalytics(data, 'reactions');
   const comments = sumAnalytics(data, 'comments');
   const follows = sumAnalytics(data, 'follows');
 
+  // Selects the HTML to be modified 
   const reactionCard = document.getElementById('reactions-card');
   const commentCard = document.getElementById('comments-card');
   const followerCard = document.getElementById('followers-card');
   const readerCard = document.getElementById('readers-card');
 
+  //Changes the innerHTML of the cards to display the following headers and stats
   readerCard.innerHTML = cardHTML(readers, `Readers ${timeRangeLabel}`);
   commentCard.innerHTML = cardHTML(comments, `Comments ${timeRangeLabel}`);
   reactionCard.innerHTML = cardHTML(reactions, `Reactions ${timeRangeLabel}`);
   followerCard.innerHTML = cardHTML(follows, `Followers ${timeRangeLabel}`);
 }
 
+
+// Chart.js function 
 function drawChart({ canvas, title, labels, datasets }) {
   const options = {
     legend: {
@@ -73,9 +81,13 @@ function drawChart({ canvas, title, labels, datasets }) {
   });
 }
 
+
 function drawCharts(data, timeRangeLabel) {
   const labels = Object.keys(data);
+  // Object.entries stringifies key value pairs
+  // parsedData grabs whatever is at the second entry slot for each bit of data
   const parsedData = Object.entries(data).map(date => date[1]);
+  // these variables are pulling out different values from that data
   const comments = parsedData.map(date => date.comments.total);
   const reactions = parsedData.map(date => date.reactions.total);
   const likes = parsedData.map(date => date.reactions.like);
@@ -84,6 +96,7 @@ function drawCharts(data, timeRangeLabel) {
   const followers = parsedData.map(date => date.follows.total);
   const readers = parsedData.map(date => date.page_views.total);
 
+  // Chart JS to fill out charts for the data extracted in the variables above 
   drawChart({
     canvas: document.getElementById('reactions-chart'),
     title: `Reactions ${timeRangeLabel}`,
@@ -167,7 +180,9 @@ function drawCharts(data, timeRangeLabel) {
 }
 
 function renderReferrers(data) {
+  // grabs the referrers container div
   const container = document.getElementById('referrers-container');
+  // prints a table row with the referrer domain and count for each referrer object 
   const tableBody = data.domains
     .filter(referrer => referrer.domain)
     .map(referrer => {
@@ -183,6 +198,7 @@ function renderReferrers(data) {
   const emptyDomainReferrer = data.domains.filter(
     referrer => !referrer.domain,
   )[0];
+  // If there are referrers with empty domains, it prints a table row stating how many miscellanious referrers there are 
   if (emptyDomainReferrer) {
     tableBody.push(`
       <tr>
@@ -196,16 +212,19 @@ function renderReferrers(data) {
 }
 
 function callAnalyticsAPI(date, timeRangeLabel, { organizationId, articleId }) {
+  // Makes an API call for the specified date to the user data
   callHistoricalAPI(date, { organizationId, articleId }, data => {
     writeCards(data, timeRangeLabel);
     drawCharts(data, timeRangeLabel);
   });
 
+  // Makes an API call to get referrer data for a given date and renders it 
   callReferrersAPI(date, { organizationId, articleId }, data => {
     renderReferrers(data);
   });
 }
 
+// Shows data for the past week
 function drawWeekCharts({ organizationId, articleId }) {
   resetActive(document.getElementById('week-button'));
   const oneWeekAgo = new Date();
@@ -213,6 +232,7 @@ function drawWeekCharts({ organizationId, articleId }) {
   callAnalyticsAPI(oneWeekAgo, 'this Week', { organizationId, articleId });
 }
 
+// Shows data for the past month
 function drawMonthCharts({ organizationId, articleId }) {
   resetActive(document.getElementById('month-button'));
   const oneMonthAgo = new Date();
@@ -220,6 +240,7 @@ function drawMonthCharts({ organizationId, articleId }) {
   callAnalyticsAPI(oneMonthAgo, 'this Month', { organizationId, articleId });
 }
 
+// Shows data for all history
 function drawInfinityCharts({ organizationId, articleId }) {
   resetActive(document.getElementById('infinity-button'));
   // April 1st is when the DEV analytics feature went into place
@@ -227,6 +248,7 @@ function drawInfinityCharts({ organizationId, articleId }) {
   callAnalyticsAPI(beginningOfTime, '', { organizationId, articleId });
 }
 
+// initializes the charts with the week, month, and infinity button options 
 export default function initCharts({ organizationId, articleId }) {
   const weekButton = document.getElementById('week-button');
   weekButton.addEventListener(
