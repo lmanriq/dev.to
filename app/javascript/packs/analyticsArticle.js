401d341ad88835b504c475dd899920021c6b7e15
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-10 13:50:08 -0600

    Annotate files that initialize the charts from dashboard.js

--- app/javascript/packs/analyticsArticle.js
+++ app/javascript/packs/analyticsArticle.js
@@ -1,5 +1,6 @@
 import initCharts from '../analytics/dashboard';
 
+// initializes the charts for the dashboard article using the exported function from the dashboard.js file
 function initDashboardArticle() {
   const article = document.getElementById('article');
   const { articleId, organizationId } = article.dataset;
--- app/javascript/packs/analyticsDashboard.js
+++ app/javascript/packs/analyticsDashboard.js
@@ -2,6 +2,7 @@ import initCharts from '../analytics/dashboard';
 
 function initDashboard() {
   const activeOrg = document.querySelector('.organization.active');
+  // sets the organixation ID for the users organization if present, doesn't initialize the charts with an article ID 
   if (activeOrg) {
     initCharts({ organizationId: activeOrg.dataset.organizationId });
   } else {
