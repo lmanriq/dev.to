7775951c04672dcd5f9cace72fb3c0ef62b32ce8
Lili Manrique <liliana.manrique1@gmail.com>
2020-05-16 13:28:39 -0600

    Delete unused comments

--- app/javascript/packs/statsDashboard.jsx
+++ app/javascript/packs/statsDashboard.jsx
@@ -1,14 +1,4 @@
-// <p>Number of total visits to the site: <span id="total-visits"></span></p>
-// <p>Number of visits per day: <span id="daily-visits"></span></p>
-// <p>Average active time on pages per day: <span id="avg-daily-time"></span></p>
-// <p>Total articles read: <span id="total-articles"></span></p>
-// <p>Total words read: <span id="total-words"></span></p>
-// <p>Average articles & words per day: <span id="avg-daily-articles-words"></span></p>
-// <p>Number of comments: <span id="total-comments"></span></p>
-
-// const totalVisits = document
 import PropTypes from 'prop-types';
-// import { userPropTypes } from '../src/components/common-prop-types';
 
 import { h, render } from 'preact';
 
